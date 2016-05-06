1c3899fec11d2c795ff5341c4429ca6f637505f0
alceo <alceo@alceo.ru>
2016-05-06 16:17:29 +0300

    add bd migrate

--- db/migrate/20160505115922_create_products.rb
+++ db/migrate/20160505115922_create_products.rb
@@ -1,15 +1,18 @@
 class CreateProducts < ActiveRecord::Migration
   def change
-	  	create_table :products do |t|
-  		t.string :title
-  		t.text :description
-  		t.text :price
-  		t.text :size
-  		t.text :weight
-  		t.boolean :is_veg
-  		t.boolean :is_best_offer
-  		t.string :path_to_image 	
-  		t.timestamps	
+
+  	create_table :products do |t|
+		t.string :title
+		t.text :description
+		t.decimal :price
+		t.decimal :size
+		t.boolean :is_spicy
+		t.boolean :is_veg
+		t.boolean :is_best_offer
+		t.string :path_to_image
+
+  		t.timestamps
+  	end
+
   end
 end
-
