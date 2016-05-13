875ba53569f15159cce427090bc54c7ac50bc016
alceo <alceo@alceo.ru>
2016-05-13 16:44:12 +0300

    add cart

---
+++ orders.rb
@@ -0,0 +1,10 @@
+orders = "product_1=5,product_2=8,product_3=2,"
+s1 = orders.split(/,/)
+puts s1.inspect
+s1.each do |s|
+	s2 = s.split(/=/)
+	s3=s2[0].split(/_/)
+	key=s3[1]
+	value=s2[1]
+	puts "Prod #{key} - #{value}"
+end
